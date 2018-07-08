cf4d04b4d72b036327b025d18746a4eccd08b6bf
vexleet <vex_malamov@abv.bg>
2018-07-08 20:49:32 +0300

    Added a paddle

--- scripts/script.js
+++ scripts/script.js
@@ -6,7 +6,12 @@ window.onload = function(){
     var dx = 2;
     var dy = -2;
     var ballRadius = 10;
-    var currentColor = "#0095DD"
+    var currentColor = "#0095DD";
+    var paddleHeight = 10;
+    var paddleWidth = 75;
+    var paddleX = (canvas.width - paddleWidth) / 2;
+    var rightPressed = false;
+    var leftPressed = false;
 
     function getRandomColor() {
         var letters = '0123456789ABCDEF';
@@ -25,9 +30,19 @@ window.onload = function(){
         ctx.closePath();
     }
 
+    function drawPaddle(){
+        ctx.beginPath();
+        ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();
+    }
+
     function draw(){
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+
         drawBall();
+        drawPaddle();
 
         if(x + dx < ballRadius || x + dx > canvas.width - ballRadius){
             dx = -dx;
@@ -40,6 +55,35 @@ window.onload = function(){
 
         x += dx;
         y += dy;
+
+        if(rightPressed && paddleX < canvas.width - paddleWidth){
+            paddleX += 5;
+        }
+        if(leftPressed && paddleX > 0){
+            paddleX -= 5;
+        }
+
+    }
+
+    document.addEventListener("keydown", keyDownHandler, false);
+    document.addEventListener("keyup", keyUpHandler, false);
+
+    function keyDownHandler(e){
+        if(e.keyCode === 39){
+            rightPressed = true;
+        }
+        else if(e.keyCode === 37){
+            leftPressed = true;
+        }
+    }
+
+    function keyUpHandler(e){
+        if(e.keyCode === 39){
+            rightPressed = false;
+        }
+        else if(e.keyCode === 37){
+            leftPressed = false;
+        }
     }
 
     setInterval(draw, 10);
