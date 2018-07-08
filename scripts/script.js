774665c3694726e05cdd7c4849fb6de869578acd
vexleet <vex_malamov@abv.bg>
2018-07-08 21:28:18 +0300

    Added bricks

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,53 @@
 window.onload = function(){
-    var canvas = document.getElementById("myCanvas");
-    var ctx = canvas.getContext("2d");
-    var x = canvas.width/2;
-    var y = canvas.height-30;
-    var dx = 2;
-    var dy = -2;
-    var ballRadius = 10;
-    var currentColor = "#0095DD";
-    var paddleHeight = 10;
-    var paddleWidth = 75;
-    var paddleX = (canvas.width - paddleWidth) / 2;
-    var rightPressed = false;
-    var leftPressed = false;
+    let canvas = document.getElementById("myCanvas");
+    let ctx = canvas.getContext("2d");
+    let x = canvas.width/2;
+    let y = canvas.height-30;
+    let dx = 2;
+    let dy = -2;
+    let ballRadius = 10;
+    let currentColor = "#0095DD";
+    let paddleHeight = 10;
+    let paddleWidth = 75;
+    let paddleX = (canvas.width - paddleWidth) / 2;
+    let rightPressed = false;
+    let leftPressed = false;
+    var brickRowCount = 3;
+    var brickColumnCount = 5;
+    var brickWidth = 75;
+    var brickHeight = 20;
+    var brickPadding = 10;
+    var brickOffsetTop = 30;
+    var brickOffsetLeft = 30;
+    let bricks = [];
 
+    for (let c = 0; c < brickColumnCount; c++) {
+        bricks[c] = [];
+        for (let r = 0; r < brickRowCount; r++){
+            bricks[c][r] = {x: 0, y: 0};
+        }
+    }
+
+    function drawBricks(){
+        for (let c = 0; c < brickColumnCount; c++){
+            for (let r = 0; r < brickRowCount; r++){
+                var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+                var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = "#0095DD";
+                ctx.fill();
+                ctx.closePath();
+            }
+        }
+    }
+    
     function getRandomColor() {
-        var letters = '0123456789ABCDEF';
-        var color = '#';
-        for (var i = 0; i < 6; i++) {
+        let letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
             color += letters[Math.floor(Math.random() * 16)];
         }
         return color;
@@ -41,6 +72,7 @@ window.onload = function(){
     function draw(){
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+        drawBricks();
         drawBall();
         drawPaddle();
 
